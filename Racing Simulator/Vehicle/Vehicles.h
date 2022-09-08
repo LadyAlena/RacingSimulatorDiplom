b4d12476fe93685e7600cc9adedbb22f2b8f1b10
Елена Степанова <lady.alena-he2015@yandex.ru>
2022-09-08 21:51:30 +0300

    update Vehicles.h

--- Racing Simulator/Vehicle/Vehicles.h
+++ Racing Simulator/Vehicle/Vehicles.h
@@ -1,12 +1,12 @@
 ﻿#pragma once
 
-#include "Ground Vehicle/BootsAllTerrain/BootsAllTerrain.h"
-#include "Air Vehicle/Broomstick/Broomstick.h"
-#include "Ground Vehicle/Camel/Camel.h"
-#include "Ground Vehicle/Centaur/Centaur.h"
-#include "Air Vehicle/Eagle/Eagle.h"
-#include "Ground Vehicle/FastMovingCamel/FastMovingCamel.h"
-#include "Air Vehicle/MagicCarpet/MagicСarpet.h"
+#include "Racing Simulator/Vehicle/Ground Vehicle/BootsAllTerrain/BootsAllTerrain.h"
+#include "Racing Simulator/Vehicle/Air Vehicle/Broomstick/Broomstick.h"
+#include "Racing Simulator/Vehicle/Ground Vehicle/Camel/Camel.h"
+#include "Racing Simulator/Vehicle/Ground Vehicle/Centaur/Centaur.h"
+#include "Racing Simulator/Vehicle/Air Vehicle/Eagle/Eagle.h"
+#include "Racing Simulator/Vehicle/Ground Vehicle/FastMovingCamel/FastMovingCamel.h"
+#include "Racing Simulator/Vehicle/Air Vehicle/MagicCarpet/MagicСarpet.h"
 
 namespace race_sim {
 	struct Vehicles {
